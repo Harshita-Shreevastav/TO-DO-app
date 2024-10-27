6021b47a5b5e276d179b69f0826b7168df720129
Harshita Shreevastav <hshrtv1101@gmail.com>
2024-10-27 13:46:03 +0545

    It now updates the tasks too! yayyyyy!

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template, redirect, request, url_for, session, flash
 from flask_sqlalchemy import SQLAlchemy
 import time
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, validators,SubmitField
+from wtforms.validators import DataRequired
 
 
 app=Flask(__name__)
-app.secret_key="todo"
+app.config['SECRET_KEY']="todo"
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.sqlite3'  # Example using SQLite
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -17,57 +20,97 @@ db = SQLAlchemy(app)
 class Data(db.Model):
     
     _s_no=db.Column("s_no", db.Integer , primary_key=True, autoincrement=True)
-    dat=db.Column(db.DateTime, nullable=False)
+    dat=db.Column(db.DateTime, nullable=False, default=datetime.now(timezone.utc))
     task=db.Column(db.String(200), nullable=False)
-
-
-    def __init__(self, dat, task):
-        
-        self.dat=dat
-        self.task=task
-
     
 
+
+    def __init__(self, task):
+        
+       
+        self.task=task
+       
+class Taskform(FlaskForm):
+    task=StringField("The task", validators=[DataRequired()])
+    submit=SubmitField("ADD")
+
+    
+class updateform(FlaskForm):
+    updated_task=StringField("Updated_task", validators=[DataRequired()]) 
+    submit= SubmitField("UPDATE")
 
 
 
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    if request.method=="POST":
 
-        print("Task entered")
-        task=request.form.get("task")
+    task=None
+    form=Taskform()
+
+    if form.validate_on_submit():
+        task= form.task.data
+        form.task.data=None
+        
+
+
+
         if task:
             
             print(task)
+            record=Data(task=task)
+            print(record)
+            
+            db.session.add(record)
+            db.session.commit()
+            print("record added")
+            flash("Task added!", "info")
+            print(record)
+            print(record.dat)
+            print(record.task)
+            
+            
+            
+           
 
-        else:
-            flash("Task not entered!", "error")
             return redirect(url_for("home"))
-        time=datetime.now()
-        print(time)
-        
-        
 
-        record=Data(time,task)
-        print(record)
-       
-        db.session.add(record)
+    else:
+
+        
+ 
+        data_records = Data.query.all()
+        print(data_records)
+
+        return render_template("index.html", data_record=data_records, form=form,task=task)
+
+
+
+
+@app.route("/update/<s_no>", methods=['GET','POST'])
+def update(s_no):
+    updated_task=None
+    update_form=updateform()
+
+    
+    record_to_update=Data.query.get_or_404(s_no)
+    print("Record found!")
+
+    if update_form.validate_on_submit():
+        updated_task= update_form.updated_task.data
+        print(updated_task)
+        record_to_update.task=updated_task
+        update_form.updated_task.data=None
+        
         db.session.commit()
-        print("record added")
-        flash("Task added!", "info")
-        print(record)
-        print(record.dat)
-        print(record.task)
         
-        # print(record.task, record.time)
         return redirect(url_for("home"))
+    
+    else:
 
-    data_records = Data.query.all()
-    print(data_records)
-    Data.query.filter_by(dat=None).delete()
-    return render_template("index.html", data=data_records)
+        print("Into else")
+        return render_template("update.html", s_no=s_no, form=update_form, updated_task=updated_task)
+    
 
 if __name__=="__main__":
     with app.app_context():
